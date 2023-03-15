59498fc1d7bdc14c6b940782d925c0e99b831530
poojamehta <poojamehta1197@gmail.com>
2023-03-15 08:04:35 +0530

    Add logs in homePage

--- pageObjects/HomePage.js
+++ pageObjects/HomePage.js
@@ -3,13 +3,19 @@ const { expect } = require("@playwright/test");
 class HomePage{
     constructor(page) {
         this.page = page;
-        this.logoLoc = page.locator(`//a[@id='nava']`);
+        this.logo = page.locator(`//a[@id='nava']`);
+        this.product = page.locator(`//*[contains(text(),'Samsung galaxy s6')]`);
+        this.addToCart = page.locator(`//*[contains(text(),'Add to cart')]`);
+        this.actualPageUrl = 'https://www.demoblaze.com/prod.html?idp_=1';
+
       }
 
     async navigateToHomePageAndValidate(page){
+        console.log("********** start navigateToHomePageAndValidate ********** \n")
         await page.goto("https://www.demoblaze.com/index.html");
         await expect(page).toHaveTitle("STORE");
-        await expect(this.logoLoc).toBeVisible();
+        await expect(this.logo).toBeVisible();
+        console.log("********** finish navigateToHomePageAndValidate ********** \n")
     }
 }
 
