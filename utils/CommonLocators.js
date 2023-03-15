c4c2e68b409e68b535b096a56fe5b81b7ab3cf46
poojamehta <poojamehta1197@gmail.com>
2023-03-15 12:43:27 +0530

    Add some common locator utils

--- utils/CommonLocators.js
+++ utils/CommonLocators.js
@@ -4,39 +4,74 @@ class CommonLocators{
     }
 
 
-    // async getWebElementByRole(page, type, locatorName, exactValue){
-    // console.log(`Onto method :: getWebElementByRole`)
-    //     console.log(`Retrieve WebElement Having type : ${type} Along With LocatorName : ${locatorName}  Along With exact : ${exactValue}`)
-    //             return await page.getByRole(type, { name: locatorName, exact:exactValue })
-        
-    // }
-
     async getWebElementByRole(page, type, locatorName){
-        console.log(`Onto method :: getWebElementByRole`)
-            console.log(`Retrieve WebElement Having type : ${type} Along With LocatorName : ${locatorName}`)
-                    return await page.getByRole(type, { name: locatorName})      
+    
+        console.log(`********** Start getWebElementByRole **********\n`)
+        console.log(`Getting WebElement with type : ${type}, LocatorName : ${locatorName}`)
+        console.log(`********** Finish getWebElementByRole **********\n`)
+                return await page.getByRole(type, { name: locatorName})  
+        
+    
+           
         }
     
 
     async clickOnWebElement(page, locator){
-        console.log(`Onto method :: clickOnWebElement`)
+       
+        console.log(`********** Start clickOnWebElement ********** \n`)
         console.log(`Click On WebElement having locator identifier as ${locator}`)
         await page.locator(`${locator}`).click()
+        console.log(`********** Finish clickOnWebElement ********** \n`)
+    
+       
     }
 
     async getWebElementByPlaceholder(page, placeHolderName){
-        console.log(`Onto method :: getWebElementByPlaceholder`)
+     
+        console.log(`********** Start getWebElementByPlaceholder ********** \n`)
+        console.log(`Getting WebElement with PlaceHolderName : ${placeHolderName}`)
+        console.log(`********** Finish getWebElementByPlaceholder **********  \n`)
+        return await page.getByPlaceholder(placeHolderName) 
          
-        console.log(`Retrieve WebElement Having PlaceHolderName : ${placeHolderName}`)
-        return await page.getByPlaceholder(placeHolderName)   
+        
+
     }
 
     async getWebElementByXPathBasedOnTagNameValue(page, tagName, LocatorName ,LocatorValue){
-        console.log(`Onto method :: getWebElementByXPath`)
-        
-        console.log(`Retrieve WebElement Having tagName : ${tagName},LocatorName : ${LocatorName},LocatorValue: ${LocatorValue}`)
+        console.log(`********** Start getWebElementByXPathBasedOnTagNameValue  **********\n`)     
+        console.log(`Getting WebElement Having tagName : ${tagName},LocatorName : ${LocatorName},LocatorValue: ${LocatorValue}`)
+        console.log(`********** Finish getWebElementByXPathBasedOnTagNameValue ********** \n`)
         return await page.locator(`//${tagName}[contains(@${LocatorName}, \'${LocatorValue}\')]`)
-            }
+             
+       
+    }
+
+    async getWebElementByLabel(page, label){
+        
+        console.log(`********** Start getWebElementByLabel ********** \n`)     
+        console.log(`Getting WebElement Having label : ${label}`)
+        console.log(`********** Finish getWebElementByLabel ********** \n`)
+        return await page.getByLabel(label)
+          
+    }
+
+    async inputTextForWebElement(webElement, inputText){
+       
+        console.log(`********** Start inputTextForWebElement ********** \n`) 
+        console.log(`Input WebElement : ${webElement} With Text Input as ${inputText}`)
+        await webElement.fill(inputText)
+        console.log(`********** Finish inputTextForWebElement ********** \n`) 
+   
+    }
+
+    async getTextContentForWebElement(webElement){
+        console.log(`Getting textContent for the WebElement : ${webElement}`)
+        const textContent = await webElement.textContent()
+        console.log(`The Text Content is : ${textContent}`)
+        console.log(`********** Finish getTextContentForWebElement ********** \n`) 
+        return await textContent             
+    }
+    
 }
 
 module.exports = CommonLocators;
