3aca4c185d274da628f2c270b9efe21b58aaca2a
poojamehta <poojamehta1197@gmail.com>
2023-03-15 08:06:16 +0530

    Add some homePage testcases

--- tests/homePage.spec.js
+++ tests/homePage.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import HomePage from '../pageObjects/HomePage';
 
 test('navigate to homePage and validate', async ({ page }) => {
