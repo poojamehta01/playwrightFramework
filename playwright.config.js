dbc9897e15fc414a9b7b6fa3f2fe6c06f7dfadec
poojamehta <poojamehta1197@gmail.com>
2023-03-15 12:47:04 +0530

    Add launchOptions

--- playwright.config.js
+++ playwright.config.js
@@ -33,7 +33,11 @@ module.exports = defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    video : 'on',
+    launchOptions :{
+      slowMo: 1000
+    },
+    /* Maximum  time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
