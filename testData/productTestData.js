a4e15e230e3b3c752e7abb061022fc8893185d4f
poojamehta <poojamehta1197@gmail.com>
2023-03-20 11:47:29 +0530

    Add testData

---
+++ testData/productTestData.js
@@ -0,0 +1,24 @@
+module.exports = {
+  PHONE: {
+    SAMSUNG_GALAXY_S6: "Samsung galaxy s6",
+    NOKIA_LUMIA_1520: "Nokia lumia 1520",
+    NEXUS_6: "Nexus 6",
+    SAMSUNG_GALAXY_S7: "Samsung galaxy s7",
+    IPHONE_6_32GB: "Iphone 6 32gb",
+    SONY_XPERIA_Z5: "Sony xperia z5",
+    HTC_ONE_M9: "HTC One M9",
+  },
+
+  LAPTOP: {
+    SONY_VAIO_I5: "Sony vaio i5",
+    SONY_VAIO_I7: "Sony vaio i7",
+    MACBOOK_AIR: "MacBook air",
+    DELL_I7_8GB: "Dell i7 8gb",
+    DELL_2017_15_6_INCH: "2017 Dell 15.6 Inch",
+    MACBOOK_PRO: "MacBook Pro",
+  },
+  MONITORS: {
+    APPLE_MONITOR_24: "Apple monitor 24",
+    ASUS_FULL_HD: "ASUS Full HD",
+  },
+};
